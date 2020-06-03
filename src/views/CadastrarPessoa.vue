7c867a6a4e24ee0b1f8ab3eb5a791626172b9836
ViniciusNunes0 <viniciusnb14@gmail.com>
2020-06-03 16:05:53 -0300

    modelo-argon

---
+++ src/views/CadastrarPessoa.vue
@@ -0,0 +1,97 @@
+<template>
+    <section class="section section-shaped section-lg my-0">
+        <div class="shape shape-style-1 bg-gradient-default">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+                        <template>
+                            <div class="text-muted text-center mb-3">
+                                <small>Cadastar Pessoa</small>
+                            </div>
+                            <!-- <div class="btn-wrapper text-center">
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/github.svg">
+                                    Github
+                                </base-button>
+
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/google.svg">
+                                    Google
+                                </base-button>
+                            </div> -->
+                        </template>
+                        <template>
+                            <!-- <div class="text-center text-muted mb-4">
+                                <small>Or sign up with credentials</small>
+                            </div> -->
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Nome"
+                                            addon-left-icon="ni ni-single-02">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="CPF"
+                                            addon-left-icon="ni ni-badge">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="RG"
+                                            addon-left-icon="ni ni-badge">
+                                </base-input>
+                                 <base-input alternative
+                                            class="mb-3"
+                                            placeholder="CNS"
+                                            addon-left-icon="ni ni-badge">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Telefone"
+                                            addon-left-icon="ni ni-mobile-button">
+                                </base-input>
+                               
+                                <!-- <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open">
+                                </base-input> -->
+                                <!-- <div class="text-muted font-italic">
+                                    <small>password strength:
+                                        <span class="text-success font-weight-700">strong</span>
+                                    </small>
+                                </div> -->
+                                <!-- <base-checkbox>
+                                    <span>I agree with the
+                                        <a href="#">Privacy Policy</a>
+                                    </span>
+                                </base-checkbox> -->
+                                <div class="text-center">
+                                    <base-button type="primary" class="my-4">Cadratrar</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
